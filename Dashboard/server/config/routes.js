84ba2ead7c4afec28cc44008b93f1059f586df75
jkinsfat <jasonrkinsfather@gmail.com>
2018-05-03 15:00:28 -0700

    add airtable api and abiliyto retrieve data from tables

--- Dashboard/server/config/routes.js
+++ Dashboard/server/config/routes.js
@@ -1,10 +1,12 @@
 
 const path = require('path');
+const db = require('../airtableController');
+
 
 module.exports = function(app) {
     //app.post('/login', myfunc)
     //app.get('/name', myfunc)
-
+    app.get('/marketing', db.getUserRes);
     app.all("*", (req, res, next) => {
         res.sendFile(path.resolve("./../public/dashboardIndex.html"))
     });
