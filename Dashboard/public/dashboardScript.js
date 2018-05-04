672a86d7e6a9306e66a10819c14f6c067268683b
tbruech <tabruecher@gmail.com>
2018-05-04 02:14:02 -0700

    done for the night

--- Dashboard/public/dashboardScript.js
+++ Dashboard/public/dashboardScript.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
-    $.get("/marketing").done(function(data) {
+    /*$.get("/marketing").done(function(data) {
         console.log(data);
+    })*/
+
+    $('.all-metrics-top').hide();
+
+    $('.all-metrics').click(function(){
+        $(this).addClass('selected');
+        $('.overview').removeClass('selected');
+        $('.conversion-results-top').hide();
+        $('#results-bottom').hide();
+        $('.all-metrics-top').show();
     })
+
+    $('.overview').click(function(){
+        $(this).addClass('selected');
+        $('.all-metrics').removeClass('selected');
+        $('.conversion-results-top').show();
+        $('#results-bottom').show();
+        $('.all-metrics-top').hide();
+    })
+
 })
