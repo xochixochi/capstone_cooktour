5bcb645df1840c36dd09447dee356d7b0d0d9086
jkinsfat <jasonrkinsfather@gmail.com>
2018-05-18 12:12:42 -0700

    prepared for all metric api calls

--- Dashboard/server/DatabaseController.js
+++ Dashboard/server/DatabaseController.js
@@ -1,22 +1,23 @@
 const Airtable = require('airtable')
 const cred = require('./config/config')
 
+//Connect to Airtable base using api credentials
 let base = new Airtable({apiKey: cred.ATB_API_KEY}).base(cred.BASE_ID);
 
 module.exports = {
+    //Retrieves all unique categorical values from airtable for each column
     getCategoryLabels : function(req, res) {
-        let categoryLabels = { 
-                "Content Type" : ["Video", "Post", "Article"],
-                "Call To Action" : ["Travel The World From Home"]
-            },
-            fields = ["Content Category", "Target Audience"],
+        let categoryLabels = {},
+            fields = ["Content Category", "Target Audience", "Content Type", "Call To Action"],
             allRecords = [];
+
         base('Capstone Ad Data (April 25)').select({"fields" : fields}).eachPage(
             (records, getNextPage) => {
                 allRecords = allRecords.concat(records);
                 getNextPage();
             },
             (error) => {
+                console.log(error)
                 allRecords.forEach( record => {
                     fields.forEach( field => {
                         let category = record.fields[field]
@@ -35,8 +36,9 @@ module.exports = {
             }
         )
     },
-
-    getAdData : function(req, res) {
+    //Test function to examine api functionality
+    getTableData : function(req, res) {
+        //Table name is the argument to base
         base('User Research').select({}).firstPage((error, records) => {
             res.json({data: records})
         })
---
+++ Dashboard/server/config/metricMap.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "ENGAGEMENT" : [
+        "PostEngagement",
+        "PageEngagement",
+    ],
+    "TRAFFIC" : [
+        "ClickThroughRate",
+        "LinkClicks",
+        "CostPerClick",
+    ],
+    "CONVERSION" : [
+        "ConversionNumber"
+    ]
+}
